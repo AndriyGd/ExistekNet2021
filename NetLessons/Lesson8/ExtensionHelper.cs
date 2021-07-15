f88f44fff446a8e2194f9ed4eed1cd7d79490958
Andrew Hladkyi <andriy.gladkyi@existek.com>
2021-07-15 19:48:07 +0300

    Lesson8

---
+++ NetLessons/Lesson8/ExtensionHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    namespace ExtensionLesson8
+    {
+        public static class StringHelper
+        {
+            public static int CountWords(this string text)
+            {
+                var temp = text.Split(' ');
+                return temp.Length;
+            }
+        }
+
+        public static class BankHelper
+        {
+            public static void AddPhone(this Bank bank, Phone phone)
+            {
+                bank.Phone = phone;
+            }
+        }
+    }
+}
---
+++ NetLessons/Lesson8/PhoneEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public partial class Phone
+    {
+        partial void OnCreated()
+        {
+            Console.WriteLine($"Phone '{Name}' created");
+        }
+    }
+}
